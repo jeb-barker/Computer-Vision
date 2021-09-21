3046d3af4f0ad383d4304f3b937330f0309d9d5e
jeebtheweeb <48389379+jeebtheweeb@users.noreply.github.com>
2021-09-20 21:58:49 -0400

    finished Lab2 Part 1

--- l021.cpp
+++ l021.cpp
@@ -1,5 +1,6 @@
 //Jeb Barker 9/14/21
 #include<iostream>
+#include<iomanip>
 #include<fstream>
 #include<stdlib.h>
 #include<vector>
@@ -9,58 +10,6 @@
 #define SIZE 800
 
 using namespace std;
-class Line{
-    private:
-        Point *a, *b;
-        double dx;
-        double dy;
-        double m;
-    public:
-        Line(){}
-        Line(double x1, double y1, double x2, double y2){
-            a = new Point(x1, y1);
-            b = new Point(x2, y2);
-            dx = x2 - x1;
-            dy = y2 - y1;
-            m = dy / dx;
-        }
-        double distance(){
-            return sqrt((pow((dx), 2.0)+pow((dy), 2.0)));
-        }
-        double getdx(){
-            return dx;
-        }
-        double getdy(){
-            return dy;
-        }
-        Point* getA(){
-            return a; 
-        }
-        Point* getB(){
-            return b;
-        }
-    //!must delete pointer after done! Finds intersection point between this Line and other Line.
-    //Uses vector algebra to find intersection
-    Point* intersection(Line other){
-        // This Line represented as a1x + b1y = c1
-        double a1 = dy;
-        double b1 = -dx;
-        double c1 = a1*((*a).getx()) + b1*((*a).gety());
-
-        // other Line represented as a2x + b2y = c2
-        double a2 = other.getdy();
-        double b2 = -1 * other.getdx();
-        double c2 = a2*((*other.getA()).getx())+ b2*((*other.getA()).gety());
-        double determinant = a1*b2 - a2*b1;
-        (b2*c1 - b1*c2)/determinant;
-        (a1*c2 - a2*c1)/determinant;
-        Point* ret = new Point((b2*c1 - b1*c2)/determinant, (a1*c2 - a2*c1)/determinant);
-        return ret;
-    }
-    ~Line(){
-        delete a, b;
-    }
-};
 class Point{
     private:
         double x, y;
@@ -81,6 +30,61 @@ class Point{
         }
         ~Point(){}
 };
+class Line{
+    private:
+        Point a, b;
+        double dx;
+        double dy;
+        double m;
+    public:
+        Line(){}
+        Line(double x1, double y1, double x2, double y2){
+            a = Point(x1, y1);
+            b = Point(x2, y2);
+            dx = x2 - x1;
+            dy = y2 - y1;
+            m = dy / dx;
+        }
+        double distance(){
+            return sqrt((pow((dx), 2.0)+pow((dy), 2.0)));
+        }
+        double getdx(){
+            return dx;
+        }
+        double getdy(){
+            return dy;
+        }
+        Point getA(){
+            return a; 
+        }
+        Point getB(){
+            return b;
+        }
+    //!must delete pointer after done! Finds intersection point between this Line and other Line.
+    //Uses vector algebra to find intersection
+    Point intersection(Line other){
+        // This Line represented as a1x + b1y = c1
+        double a1 = dy;
+        double b1 = -dx;
+        double c1 = a1*(a.getx()) + b1*(a.gety());
+
+        // other Line represented as a2x + b2y = c2
+        double a2 = other.getdy();
+        double b2 = -1 * other.getdx();
+        double c2 = a2*(other.getA().getx())+ b2*(other.getA().gety());
+        double determinant = a1*b2 - a2*b1;
+        if(determinant == 0){
+            return Point(5.0, 5.0);
+        }
+        (b2*c1 - b1*c2)/determinant;
+        (a1*c2 - a2*c1)/determinant;
+        Point ret = Point((b2*c1 - b1*c2)/determinant, (a1*c2 - a2*c1)/determinant);
+        return ret;
+    }
+    ~Line(){
+    }
+};
+
 class Canvas{
     private:
         vector<Point>* polygonPoints;
@@ -93,7 +97,8 @@ class Canvas{
         }
 
         double distance(Point a, Point b){
-            Line a = Line(a.getx(), a.gety(), b.getx(), b.gety());
+            Line d = Line(a.getx(), a.gety(), b.getx(), b.gety());
+            return d.distance();
         }
 
         //Helper Method --> The Bresenham algorithm that works in all cases.
@@ -191,3 +196,59 @@ class Canvas{
             delete [] grid;
         }
 };
+
+int main()
+{
+    //seed the random number generator based on current time (makes it more random.)
+    srand(time(NULL));
+    Canvas *t = new Canvas();
+    vector<Line>* lines = new vector<Line>();
+    vector<Point>* points;
+    points = (*t).generatePoints(3);
+    (*points).push_back((*points)[0]);
+    for(int x = 0; x < 3; x++){
+        Line a = Line((*points)[x].getx(), (*points)[x].gety(), (*points)[x+1].getx(), (*points)[x+1].gety());
+        (*lines).push_back(a);
+    }
+    (*points).pop_back();
+    bool valid = false;
+    Point testpt;
+    int intersections = 0;
+    int vertexIntersections = 0;
+    while(!valid){
+        testpt = (*t).generatept();
+        Line testLine = Line(testpt.getx(), testpt.gety(), testpt.getx()+1.0, testpt.gety());
+        for(int x = 0; x < 3; x++){
+            Point intersection = testLine.intersection((*lines)[x]);
+            if(intersection.getx() == 5.0 && intersection.gety() == 5.0){
+                intersections++;
+            }
+            if(( (*lines)[x].getA().getx() > intersection.getx() && (*lines)[x].getB().getx() > intersection.getx() ) || ( (*lines)[x].getA().getx() < intersection.getx() && (*lines)[x].getB().getx() < intersection.getx() )){
+            }
+            else{
+                intersections++;
+            }
+            if((*lines)[x].getA().getx() > intersection.getx()-0.00001 && (*lines)[x].getA().getx() < intersection.getx()+0.00001){
+                vertexIntersections++;
+            }
+        }
+        cout << "vertInts: " << vertexIntersections << endl;
+        intersections = intersections + (int)(vertexIntersections / 2);
+        if(intersections % 2 == 0){
+            valid = true;
+            (*points).push_back(testpt);
+        }
+    }
+    cout << fixed;
+    cout << setprecision(17);
+    ofstream txt;
+    txt.open("points.txt");
+    for(int x = 0; x < 4; x++){
+        txt << "(" << (*points)[x].getx() << ", " << (*points)[x].gety() << "), " << endl;
+    }
+
+    //cleanup
+    delete t;
+    delete lines, points;
+    return 0;
+}
