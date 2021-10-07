9d54866f0bbd96250663d8b1d40f808f6dda834e
jeebtheweeb <48389379+jeebtheweeb@users.noreply.github.com>
2021-10-07 15:53:12 -0400

    nearly done with smallest square
    TODO: pick the smallest square out of the 6 options.

--- l021.cpp
+++ l021.cpp
@@ -4,6 +4,7 @@
 #include<fstream>
 #include<stdlib.h>
 #include<vector>
+#include<string>
 #include <time.h>
 #include <math.h>
 
@@ -68,29 +69,35 @@ class Line{
         Point getB(){
             return b;
         }
-    //Finds intersection point between this Line and other Line.
-    //Uses vector algebra to find intersection
-    Point intersection(Line other){
-        // This Line represented as a1x + b1y = c1
-        double a1 = dy;
-        double b1 = -dx;
-        double c1 = a1*(a.getx()) + b1*(a.gety());
-
-        // other Line represented as a2x + b2y = c2
-        double a2 = other.getdy();
-        double b2 = -1 * other.getdx();
-        double c2 = a2*(other.getA().getx())+ b2*(other.getA().gety());
-        double determinant = a1*b2 - a2*b1;
-        if(determinant == 0){
-            //return a Point (5.0, 5.0) if the line is colinear. Maybe consider using NaN?
-            //There is a point to be made for either a large number like 5.0 or Nan.
-            return Point(5.0, 5.0);
+        vector<Line> perpendicular(Point base){
+            vector<Line> ret;
+            ret.push_back(Line(base.getx(), base.gety(), base.getx() + (-1 * dy), base.gety() + (dx)));
+            ret.push_back(Line(base.getx(), base.gety(), base.getx() - (-1 * dy), base.gety() - (dx)));
+            return ret;
         }
-        Point ret = Point((b2*c1 - b1*c2)/determinant, (a1*c2 - a2*c1)/determinant);
-        return ret;
-    }
-    ~Line(){
-    }
+        //Finds intersection point between this Line and other Line.
+        //Uses vector algebra to find intersection
+        Point intersection(Line other){
+            // This Line represented as a1x + b1y = c1
+            double a1 = dy;
+            double b1 = -dx;
+            double c1 = a1*(a.getx()) + b1*(a.gety());
+
+            // other Line represented as a2x + b2y = c2
+            double a2 = other.getdy();
+            double b2 = -1 * other.getdx();
+            double c2 = a2*(other.getA().getx())+ b2*(other.getA().gety());
+            double determinant = a1*b2 - a2*b1;
+            if(determinant == 0){
+                //return a Point (5.0, 5.0) if the line is colinear. Maybe consider using NaN?
+                //There is a point to be made for either a large number like 5.0 or Nan.
+                return Point(5.0, 5.0);
+            }
+            Point ret = Point((b2*c1 - b1*c2)/determinant, (a1*c2 - a2*c1)/determinant);
+            return ret;
+        }
+        ~Line(){
+        }
 };
 
 class Canvas{
@@ -109,8 +116,54 @@ class Canvas{
             return d.distance();
         }
 
+        void drawCircle(int x1, int y1, int r){
+            int x = 0;
+            int y = r;
+            int ty = 3 - (2*r);
+            //if(!(x1+x > SIZE || x1-x < 0 || x1 +y > SIZE || x1-y < 0 || y1+x > SIZE || y1-x < 0 || y1 +y > SIZE || y1-y < 0)){
+            if(x1+x < SIZE && y1+y < SIZE && x1+x >= 0 && y1+y >= 0){grid[x1 + x][y1 + y] = 10;}
+            if(x1+x < SIZE && y1-y < SIZE && x1+x >= 0 && y1-y >= 0){grid[x1 + x][y1 - y] = 10;}
+            if(x1-x < SIZE && y1+y < SIZE && x1-x >= 0 && y1+y >= 0){grid[x1 - x][y1 + y] = 10;}
+            if(x1-x < SIZE && y1-y < SIZE && x1-x >= 0 && y1-y >= 0){grid[x1 - x][y1 - y] = 10;}
+            if(x1+y < SIZE && y1+x < SIZE && x1+y >= 0 && y1+x >= 0){grid[x1 + y][y1 + x] = 10;}
+            if(x1-y < SIZE && y1+x < SIZE && x1-y >= 0 && y1+x >= 0){grid[x1 - y][y1 + x] = 10;}
+            if(x1+y < SIZE && y1-x < SIZE && x1+y >= 0 && y1-x >= 0){grid[x1 + y][y1 - x] = 10;}
+            if(x1-y < SIZE && y1-x < SIZE && x1-y >= 0 && y1-x >= 0){grid[x1 - y][y1 - x] = 10;}
+            //}
+
+            while(y >= x){
+                //increment clockwise
+                x++;
+                if(ty>0){
+                    y-=1;
+                    ty = ty+(4 * (x - y)) + 10;
+                }
+                else{
+                    ty = ty + (4 * x) + 6;
+                }
+                if(x1+x < SIZE && y1+y < SIZE && x1+x >= 0 && y1+y >= 0){grid[x1 + x][y1 + y] = 10;}
+                if(x1+x < SIZE && y1-y < SIZE && x1+x >= 0 && y1-y >= 0){grid[x1 + x][y1 - y] = 10;}
+                if(x1-x < SIZE && y1+y < SIZE && x1-x >= 0 && y1+y >= 0){grid[x1 - x][y1 + y] = 10;}
+                if(x1-x < SIZE && y1-y < SIZE && x1-x >= 0 && y1-y >= 0){grid[x1 - x][y1 - y] = 10;}
+                if(x1+y < SIZE && y1+x < SIZE && x1+y >= 0 && y1+x >= 0){grid[x1 + y][y1 + x] = 10;}
+                if(x1-y < SIZE && y1+x < SIZE && x1-y >= 0 && y1+x >= 0){grid[x1 - y][y1 + x] = 10;}
+                if(x1+y < SIZE && y1-x < SIZE && x1+y >= 0 && y1-x >= 0){grid[x1 + y][y1 - x] = 10;}
+                if(x1-y < SIZE && y1-x < SIZE && x1-y >= 0 && y1-x >= 0){grid[x1 - y][y1 - x] = 10;}
+
+            }
+
+        }
         //Helper Method --> The Bresenham algorithm that works in all cases.
         void bresenham_pos(int x1, int y1, int x2, int y2){
+            if(x2 - x1 < 0){
+                int yt, xt;
+                xt = x1;
+                yt = y1;
+                x1 = x2;
+                y1 = y2;
+                x2 = xt;
+                y2 = yt;
+            }
             int dx = x2 - x1;
             int dy = y2 - y1; 
             int inc = 1;
@@ -271,6 +324,8 @@ vector<vector<Point>*> part1(){
         Line testLine = Line(0.0, testpt.gety(), testpt.getx(), testpt.gety());
         bool keep_going = true;
         for(int y = 0; y < 4; y++){
+            intersections = 0;
+            vertexIntersections = 0;
             if(keep_going){
                 for(int x = 0; x < 3; x++){
                     Point intersection = testLine.intersection((*lines)[y][x]);
@@ -320,6 +375,58 @@ vector<vector<Point>*> part1(){
     return points;
 }
 
+void part2(vector<Point> pts){
+    vector<vector<Line>> lines_to_constr = vector<vector<Line>>();
+    double ax, bx, cx, dx, ay, by, cy, dy;
+    ax = pts[0].getx();
+    ay = pts[0].gety();
+    bx = pts[1].getx();
+    by = pts[1].gety();
+    cx = pts[2].getx();
+    cy = pts[2].gety();
+    dx = pts[3].getx();
+    dy = pts[3].gety();
+    for(int i = 0; i < 3; i++){
+        lines_to_constr.push_back(vector<Line>());
+    }
+    lines_to_constr[0].push_back(Line(ax, ay, bx, by));
+    lines_to_constr[0].push_back(Line(dx, dy, cx, cy));
+
+    lines_to_constr[1].push_back(Line(ax, ay, cx, cy));
+    lines_to_constr[1].push_back(Line(bx, by, dx, dy));
+
+    lines_to_constr[2].push_back(Line(ax, ay, dx, dy));
+    lines_to_constr[2].push_back(Line(bx, by, cx, cy));
+
+    for(int iter = 0; iter < 3; iter++){
+        //TODO: drop perpendiculars and connect.
+  Point e = Point(lines_to_constr[iter][0].perpendicular(lines_to_constr[iter][1].getA())[1].getB().getx(), lines_to_constr[iter][0].perpendicular(lines_to_constr[iter][1].getA())[1].getB().gety());
+        lines_to_constr[iter].push_back(Line(lines_to_constr[iter][1].getB().getx(), lines_to_constr[iter][1].getB().gety(), e.getx(), e.gety()));
+        
+        e = Point(lines_to_constr[iter][0].perpendicular(lines_to_constr[iter][1].getB())[0].getB().getx(), lines_to_constr[iter][0].perpendicular(lines_to_constr[iter][1].getB())[0].getB().gety());
+        lines_to_constr[iter].push_back(Line(lines_to_constr[iter][1].getA().getx(), lines_to_constr[iter][1].getA().gety(), e.getx(), e.gety()));
+        
+        e = Point(lines_to_constr[iter][1].perpendicular(lines_to_constr[iter][0].getA())[0].getB().getx(), lines_to_constr[iter][1].perpendicular(lines_to_constr[iter][0].getA())[0].getB().gety());
+        lines_to_constr[iter].push_back(Line(lines_to_constr[iter][0].getB().getx(), lines_to_constr[iter][0].getB().gety(), e.getx(), e.gety()));
+        
+        e = Point(lines_to_constr[iter][1].perpendicular(lines_to_constr[iter][0].getB())[1].getB().getx(), lines_to_constr[iter][1].perpendicular(lines_to_constr[iter][0].getB())[1].getB().gety());
+        lines_to_constr[iter].push_back(Line(lines_to_constr[iter][0].getA().getx(), lines_to_constr[iter][0].getA().gety(), e.getx(), e.gety()));
+        Canvas p;
+        p.drawCircle((int)(ax*SIZE), (int)(ay*SIZE), 2);
+        p.drawCircle((int)(bx*SIZE), (int)(by*SIZE), 2);
+        p.drawCircle((int)(cx*SIZE), (int)(cy*SIZE), 2);
+        p.drawCircle((int)(dx*SIZE), (int)(dy*SIZE), 2);
+        for(int i = 2; i < 6; i++){
+            p.bresenham_pos((int)(lines_to_constr[iter][i].getA().getx()*SIZE), (int)(lines_to_constr[iter][i].getA().gety()*SIZE), (int)(lines_to_constr[iter][i].getB().getx()*SIZE), (int)(lines_to_constr[iter][i].getB().gety()*SIZE));
+            cout << (int)(lines_to_constr[iter][i].getA().getx()*SIZE) << endl;
+            cout << (int)(lines_to_constr[iter][i].getA().gety()*SIZE) << endl;
+            cout << (int)(lines_to_constr[iter][i].getB().getx()*SIZE) << endl;
+            cout << (int)(lines_to_constr[iter][i].getB().gety()*SIZE) << endl << endl;
+        }
+        p.draw_grid();
+    }
+}
+
 int main()
 {
     //seed the random number generator based on current time (makes it more random.)
@@ -340,10 +447,23 @@ int main()
     for(int x = 0; x < 4; x++){
         txt << "(" << (*points[4])[x].getx() << ", " << (*points[4])[x].gety() << "), " << endl;
     }
-
-    //cleanup
+    txt.close();
     for (int x = 0; x < 4; x++){
         delete points[x];
     }
+
+    fstream pointFile;
+    pointFile.open("points.txt", ios::in);
+    string line;
+    vector<Point> point;
+    //read in the points from points.txt
+    while(getline(pointFile, line)){
+        point.push_back(Point(std::stod(line.substr(1, 19)), std::stod(line.substr(22, 19))));
+    }
+    //std::cout << point[0].getx() << point[0].gety();
+    part2(point);
+
+    //cleanup
+    pointFile.close();
     return 0;
 }
